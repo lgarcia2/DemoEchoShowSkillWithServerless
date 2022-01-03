9547ba7309be5ec5f9f3105d7f518abfa39893f7
ijollyman <ijollyman@yahoo.com>
2022-01-03 17:40:45 -0500

    Minor update pre-commit working

--- alexa_skill_id_check_git_hook.py
+++ alexa_skill_id_check_git_hook.py
@@ -13,12 +13,16 @@ for root, directories, files in os.walk(path, topdown=False):
             with open(file_path,'r') as file_obj:
                 for line in file_obj.readlines():
                     if 'alexaSkill' in line and 'amzn1.ask.skill.' in line:
-                        print("Don't commit the Alexa Skill Id in the serverless.yml file.")
+                        print("!! Don't commit the Alexa Skill Id in the serverless.yml file.")
                         print(f"Line: \r\n    {line}")
                         print(file_path)
                         exit(1)
-                    elif 'skillId' in line and 'amzn1.ask.skill.' in line:
-                        print("Don't commit the Alexa Skill Id in the ask-resources.json file.")
+        elif 'ask-resources.json' in name:
+            file_path = os.path.join(root, name)
+            with open(file_path,'r') as file_obj:
+                for line in file_obj.readlines():
+                    if 'skillId' in line and 'amzn1.ask.skill.' in line:
+                        print("!! Don't commit the Alexa Skill Id in the ask-resources.json file.")
                         print(f"Line: \r\n    {line}")
                         print(file_path)
                         exit(1)
